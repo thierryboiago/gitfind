4cb0ed55647258e145b6df29d0f183be93ed9da3
Thierry Boiago <thierry123123@gmail.com>
2024-10-24 23:03:04 -0300

    aula 1

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,15 @@
+import { Header } from '../../components/Header';
+
+import background from '../../assets/background.png';
+ 
 function App() {
   return (
     <div className="App">
-      
+      <Header/>
+      <div className='conteudo'>
+        <img src={background} className='background' alt='background app'/>
+
+      </div>
     </div>
   );
 }
